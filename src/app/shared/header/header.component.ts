9a22b908d156779cad3dd918e1e35c77cad21ee7
augustinjanvier <aug.j22@hotmail.fr>
2024-03-31 20:43:44 +0700

    WIP

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+  mobile: boolean = false;
+
+  menuOpen: boolean = true;
+
+  ngOnInit(): void {
+    this.mobile = window.screen.width < 600;
+  }
 
 }
