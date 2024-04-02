fca31eb8cf58b0a3baf557a9a633750c3ce1b297
augustinjanvier <aug.j22@hotmail.fr>
2024-04-02 10:07:10 +0700

    Timeline

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  @ViewChild('sideMenu') sideMenu!: ElementRef;
+
   mobile: boolean = false;
 
-  menuOpen: boolean = true;
+  menuOpen: boolean = false;
+
+  constructor(private renderer: Renderer2) {
+    this.renderer.listen('window', 'click', (e: Event) => {
+      if (e.target !== this.sideMenu.nativeElement) {
+        this.menuOpen = false;
+      }
+    });
+  }
 
   ngOnInit(): void {
     this.mobile = window.screen.width < 600;
