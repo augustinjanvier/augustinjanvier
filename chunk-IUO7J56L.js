605a0559c2f8f0cc1983d52db060b90c3be13438
augustinjanvier <aug.j22@hotmail.fr>
2024-07-09 09:57:37 +0700

    Auto-generated commit

--- chunk-IUO7J56L.js
+++
@@ -1,2 +0,0 @@
-import{a as o,b as r,c as a,g as c,h as s,m,r as d}from"./chunk-JVDY7EJV.js";var u=(()=>{let e=class e{};e.\u0275fac=function(n){return new(n||e)},e.\u0275cmp=r({type:e,selectors:[["app-footer"]],decls:2,vars:0,template:function(n,p){n&1&&(c(0,"footer"),m(1,` \xA9 Augustin Janvier - M\xE0J 08/07/2024
-`),s())},styles:["footer[_ngcontent-%COMP%]{width:100%;text-align:center}"]});let t=e;return t})();var P=(()=>{let e=class e{};e.\u0275fac=function(n){return new(n||e)},e.\u0275mod=a({type:e}),e.\u0275inj=o({imports:[d]});let t=e;return t})();export{u as a,P as b};
