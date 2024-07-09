66b3f2c28975dc1f7322a6f9d7082a5391a4ef27
augustinjanvier <aug.j22@hotmail.fr>
2024-07-09 09:43:24 +0700

    V2 done

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,10 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  showCMR: boolean = false;
+  showMyTwido: boolean = false;
+  showDinoRPG: boolean = false;
+
   constructor() {
   }
 
